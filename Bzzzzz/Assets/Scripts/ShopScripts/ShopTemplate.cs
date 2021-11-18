2185d198f9ac90d36ed264b724a4e05691addc59
natterdal <fnatterdal@gmail.com>
2021-11-18 19:28:15 +0100

    Shop scene
    Last commit was done before saving the scene.

--- Bzzzzz/Assets/Scripts/ShopScripts/ShopTemplate.cs
+++ Bzzzzz/Assets/Scripts/ShopScripts/ShopTemplate.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class ShopTemplate : MonoBehaviour
 {
+    // Class holding the text elements of each shop item to display it correctly.  
     public TMP_Text titleTxt;
     public TMP_Text quantityTxt;
     public TMP_Text costTxt;
