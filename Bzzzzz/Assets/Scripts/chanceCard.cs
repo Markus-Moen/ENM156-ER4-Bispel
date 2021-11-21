0bd5f79fda906c24dc1be4289ea93867d9886d60
linwahlg <70467935+linwahlg@users.noreply.github.com>
2021-11-21 11:04:21 +0100

    added scenes and randomisation for chancecard 1 and 2

--- Bzzzzz/Assets/Scripts/chanceCard.cs
+++ Bzzzzz/Assets/Scripts/chanceCard.cs
@@ -3,44 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class chanceCard : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public SpriteRenderer chancecard;
+    //public SpriteRenderer chancecard;
 
     void Start()
     {
+      
         
     }
 
     public void RandomizeChanceCard()
     {
         //randomizes a number to pick chancecard
-        int card = Random.Range(0, 9);
+        int card = Random.Range(0, 2); //currently only randomizes between cards that we have created scenes for
+        
+        //for debugging purpuces
         Debug.Log("card " + card);
 
-        //test code, doesn't work don't use
-        //GameObject card2 = GameObject.FindGameObjectWithTag("chanceCard#1");
-        //card2.GetComponent<Renderer>().enabled = true;
-        //card2.SetActive(true);
-        //chancecard.GetComponent<Renderer>().enabled = true;
-        //chancecard.enabled = true;
-        chancecard = GetComponent<SpriteRenderer>();
-        chancecard.enabled = true;
+        //loads new scene with chosen card
+        CardManager(card);
     }
 
-    //enables consequences for each card
+    //Handles each individual card
     public void CardManager(int card)
     {
         switch (card)
         {
             case 0:
-                GameObject card1 = GameObject.FindGameObjectWithTag("Chancecard#1");
-                card1.GetComponent<Renderer>().enabled = true;
+                //todo: add concequences eg +honey
+
+                //load scene with chance card 1
+                SceneManager.LoadScene("ChanceCard#1");
+                break;
+            case 1:
+                //todo: add concequences eg +honey
+
+                //load scene with chance card 2
+                SceneManager.LoadScene("ChanceCard#2");
                 break;
         }
+    }
+
+    //returns from card scene to store, to be used with button on card scene
+    public void ReturnToStore()
+    {
+        Debug.Log("in return to store");
+        SceneManager.LoadScene("Shop");
     }
 
 }
