9127e7cec9eb1b7b5bc494e1483ca61ffa4e77c6
Markus-Moen <moenm@student.chalmers.se>
2021-11-26 18:34:59 +0100

    Log buttons now say what they lead to.

--- Bzzzzz/Assets/Scripts/LogManager.cs
+++ Bzzzzz/Assets/Scripts/LogManager.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 public class LogManager : MonoBehaviour
 {
     public GameObject log;
-    public List<int> savedCards = new List<int>();
-    private int buttonIndex = 0;
+    private List<int> savedCards = new List<int>();
     public GameObject[] logButtons;
+    private int buttonIndex = 0;
+    public Dictionary<int, string> infoTitles = new Dictionary<int, string>();
 
     public static LogManager instance;
 
@@ -28,6 +29,10 @@ public class LogManager : MonoBehaviour
         // Deactivates all the buttons, as nothing is logged before the game starts.
         foreach(GameObject logButton in logButtons)
         { logButton.SetActive(false); }
+
+        // Titles for the buttons with the index of the corresponding card as key.
+        infoTitles.Add(10, "So Many Bees");
+        infoTitles.Add(11, "Worker Bee");
 
         // Inactivates the log while still letting it get instantiated.
         log.SetActive(false);
@@ -50,11 +55,14 @@ public class LogManager : MonoBehaviour
 
     public void saveToLog(int card)
     {
-        // Saves the card int to savedCards, activates the buttons for it,
-        // and increments the button list index
-        savedCards.Add(card);
-        logButtons[buttonIndex].SetActive(true);
-        buttonIndex++;
+        // Saves the card int to savedCards if it's not there already,
+        // activates the buttons for it, and increments the button list index
+        if (!savedCards.Contains(card)) {
+            savedCards.Add(card);
+            logButtons[buttonIndex].SetActive(true);
+            logButtons[buttonIndex].GetComponentInChildren<Text>().text = infoTitles[card];
+            buttonIndex++;
+        }
         Debug.Log("Logged card " + string.Join(",", savedCards));
     }
 
