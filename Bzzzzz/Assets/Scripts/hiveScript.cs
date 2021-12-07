459e6d361e554256921500c791d46d90c97d1389
Henny-Biander <70686784+Henny-Biander@users.noreply.github.com>
2021-12-07 23:52:44 +0100

    Food decreases when the honey storage is full
    Food still decreases when the honey storage is full, I changed the code and commented it out because I don't know what the balancing group said about food and full storage

--- Bzzzzz/Assets/Scripts/hiveScript.cs
+++ Bzzzzz/Assets/Scripts/hiveScript.cs
@@ -96,7 +96,8 @@ public class hiveScript : MonoBehaviour
     {
         //Moves honey to the "cellar", if the "cellar" is full, the leftovers are stored in the hive
             leftovers = GameManager.instance.incPlayerHoney(honeyAmount);
-            GameManager.instance.decPlayerFood(honeyAmount/10);
+            GameManager.instance.decPlayerFood((honeyAmount-leftovers)/10); //To not decrease the food when honey is full
+            //GameManager.instance.decPlayerFood(honeyAmount/10);
             honeyAmount = leftovers;
 
     }
@@ -112,6 +113,7 @@ public class hiveScript : MonoBehaviour
             //Honey in normal hives
             normalHoneyIncrease = Mathf.RoundToInt((honeyIncrease * tmpHives/totalHives));         
             leftovers = GameManager.instance.incPlayerHoney(honeyIncrease - normalHoneyIncrease);
+            //GameManager.instance.decPlayerFood((honeyIncrease - normalHoneyIncrease-leftovers)/10);//I think this should be the correct decFood
             GameManager.instance.decPlayerFood(honeyIncrease - normalHoneyIncrease);
             honeyAmount += leftovers;
             temporaryHoney = honeyAmount + normalHoneyIncrease;
